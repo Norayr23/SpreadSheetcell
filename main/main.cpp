a05c5318755109edd8151b8c98f87f2e1fc5e23e
Norayr23 <117726790+Norayr23@users.noreply.github.com>
2023-08-31 12:15:27 +0400

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -5,20 +5,69 @@
 using namespace std;
 
 int main() {
-    SpreadsheetCell s1(10, 10);
-    s1.setCell(5, 5, "0");
-    s1.print();
-    s1.addColumn(2);
-    s1.print();
-    s1.removeRow(5);
-    s1.print();
-    s1.addRow(4);
-    s1.print();
-    s1.removeColumn(1);
-    s1.print();
+    SpreadsheetCell ob;
+    ob.addRow(2); // ավելացնում է երկու տող
 
-    SpreadsheetCell s2 = move(s1);
-    s2.print();
-    s1 = s2;
-    s1.print();
+Cell* c = ob.getCell(1, 1); // nullptr, աղյուսակն ունի երկու տող, բայց չունի սյունակներ
+//cout << c->getStringValue(); // runtime error, dereferencing a nullptr
+
+ob.addColumn(2); // ավելացնում է երկու սյունակ
+ob.print();
+// այս կետում աղյուսակն ունի այս տեսքը
+/*-------
+|   |   |
+ -------
+|   |   |
+ -------*/
+
+ob.getCell(1, 1)->getStringValue(); // վերադարձնում է դատարկ տող
+ob.setCell(0, 0, "hello");
+ob.setCell(0, 1, "3.14");
+ob.setCell(1, 1, "val");
+ob.print();
+/* այս կետում աղյուսակն ունի այս տեսքը
+ ---------------------
+| "hello"  | "3.14"  |
+ ---------------------
+|          | "val"   |
+ ---------------------*/
+
+cout << ob.getCell(0, 0)->getStringValue() << endl; // տպում է "hello"
+cout << ob.getCell(1, 0)->getStringValue() << endl; // տպում է ""
+Cell* cell = ob.getCell(1, 1);
+cout << cell->getStringValue(); // տպում է "val"
+cell->setStringValue("new");
+ob.print();
+// այս կետում աղյուսակն ունի այս տեսքը
+ /*---------------------
+| "hello"  | "3.14"  |
+ ---------------------
+|          | "new"   |
+ ---------------------*/
+
+//double v = cell->getDoubleValue(); // runtime error: չի կարող կոնվերտացնել "new" բառը դեպի double
+double v2 = ob.getCell(0, 1)->getDoubleValue(); 
+cout << v2 << endl;; // տպում է 3.14
+
+Cell* intCell = ob.getCell(1, 0); // վերադարձնում է ցուցիչ դատարկ տող պարունակող cell-ի վրա
+// intCell-ը կհղվի այս cell-ին (նկարում նշված է x նշանով)
+ob.print();
+ /*---------------------
+| "hello"  | "3.14"  |
+ ---------------------
+|    x     | "new"   |
+ ---------------------*/
+
+intCell->setStringValue("99");
+ob.print();
+// այս կետում աղյուսակն ունի այս տեսքը
+/* ---------------------
+| "hello"  | "3.14"  |
+ ---------------------
+|   "99"   | "new"   |
+ ---------------------*/
+
+cout << ob.getCell(1, 0)->getIntValue() << endl;; // կտպի 99
+cout << intCell->getStringValue() << endl; // կտպի "99"
+//cout << ob.getCell(0, 0)->getIntValue(); // runtime error, չի կոնվերտացվում "hello"-ն դեպի int
 }
