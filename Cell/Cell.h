3e374fa2052f9c66cebe681468f1f9fbced55d19
Norayr23 <smbatyan.noro@mail.ru>
2023-09-01 21:01:12 +0400

    new

--- Cell/Cell.h
+++ Cell/Cell.h
@@ -2,6 +2,7 @@
 #define CELL_H
 
 #include <string>
+#include <ostream>
 
 class Cell {
 public:
@@ -12,8 +13,20 @@ public:
     int getIntValue() const;
     double getDoubleValue() const;
     bool isEmpty() const;
+public:
+    explicit operator int() const;
+    explicit operator double() const;
+    Cell& operator++();
+    Cell operator++(int);
+    Cell& operator--();
+    Cell operator--(int);    
 private:
     std::string m_value;
 };
+
+std::ostream& operator<<(std::ostream&, const Cell&);
+Cell operator+(const Cell&, const Cell&);
+Cell operator+(const Cell&, const int);
+Cell operator+(const int, const Cell&);
 
 #endif
--- main/main.cpp
+++ main/main.cpp
@@ -5,69 +5,12 @@
 using namespace std;
 
 int main() {
-    Spreadsheet ob;
-    ob.addRow(2); // ավելացնում է երկու տող
-
-Cell* c = ob.getCell(1, 1); // nullptr, աղյուսակն ունի երկու տող, բայց չունի սյունակներ
-//cout << c->getStringValue(); // runtime error, dereferencing a nullptr
-
-ob.addColumn(2); // ավելացնում է երկու սյունակ
-ob.print();
-// այս կետում աղյուսակն ունի այս տեսքը
-/*-------
-|   |   |
- -------
-|   |   |
- -------*/
-
-ob.getCell(1, 1)->getStringValue(); // վերադարձնում է դատարկ տող
-ob.setCell(0, 0, "hello");
-ob.setCell(0, 1, "3.14");
-ob.setCell(1, 1, "val");
-ob.print();
-/* այս կետում աղյուսակն ունի այս տեսքը
- ---------------------
-| "hello"  | "3.14"  |
- ---------------------
-|          | "val"   |
- ---------------------*/
-
-cout << ob.getCell(0, 0)->getStringValue() << endl; // տպում է "hello"
-cout << ob.getCell(1, 0)->getStringValue() << endl; // տպում է ""
-Cell* cell = ob.getCell(1, 1);
-cout << cell->getStringValue(); // տպում է "val"
-cell->setStringValue("new");
-ob.print();
-// այս կետում աղյուսակն ունի այս տեսքը
- /*---------------------
-| "hello"  | "3.14"  |
- ---------------------
-|          | "new"   |
- ---------------------*/
-
-//double v = cell->getDoubleValue(); // runtime error: չի կարող կոնվերտացնել "new" բառը դեպի double
-double v2 = ob.getCell(0, 1)->getDoubleValue(); 
-cout << v2 << endl;; // տպում է 3.14
-
-Cell* intCell = ob.getCell(1, 0); // վերադարձնում է ցուցիչ դատարկ տող պարունակող cell-ի վրա
-// intCell-ը կհղվի այս cell-ին (նկարում նշված է x նշանով)
-ob.print();
- /*---------------------
-| "hello"  | "3.14"  |
- ---------------------
-|    x     | "new"   |
- ---------------------*/
-
-intCell->setStringValue("99");
-ob.print();
-// այս կետում աղյուսակն ունի այս տեսքը
-/* ---------------------
-| "hello"  | "3.14"  |
- ---------------------
-|   "99"   | "new"   |
- ---------------------*/
-
-cout << ob.getCell(1, 0)->getIntValue() << endl;; // կտպի 99
-cout << intCell->getStringValue() << endl; // կտպի "99"
-//cout << ob.getCell(0, 0)->getIntValue(); // runtime error, չի կոնվերտացվում "hello"-ն դեպի int
+    Cell cell1("20");
+    Cell cell2("20");
+   try {
+     cout << (12.45 + cell1 ) << endl;
+   }
+   catch(const std::exception& a) {
+     cout << a.what() << endl;    
+   }
 }
